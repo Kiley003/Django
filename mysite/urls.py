a722d68f5553cbcea9d09777a95809c4b02d5111
Kiley003 <KileyFeist@gmail.com>
2025-02-27 17:15:15 -0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,12 +19,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-]
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-]
+    path("admin/", admin.site.urls),]
+    path('', include('blog.urls'))]
